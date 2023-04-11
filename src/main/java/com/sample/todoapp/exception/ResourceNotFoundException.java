3faa6401afee92712c91dd8d1c6593644fc6ad04
kowsalya <p.r.kowsalya149@gmail.com>
2023-04-11 11:16:07 +0530

    error message property added to properties file

--- src/main/java/com/sample/todoapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/sample/todoapp/exception/ResourceNotFoundException.java
@@ -1,8 +1,9 @@
 package com.sample.todoapp.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus//(code = HttpStatus.NOT_FOUND, reason = "Resource Not Found")
 public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
