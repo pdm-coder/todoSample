67fa2342fbf563c119a3dee866be232589e0ee07
kowsalya <p.r.kowsalya149@gmail.com>
2023-04-13 11:13:34 +0530

    controller based exception handling feature added

--- src/main/java/com/sample/todoapp/controller/TaskController.java
+++ src/main/java/com/sample/todoapp/controller/TaskController.java
@@ -8,14 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.todoapp.exception.ResourceNotFoundException;
 import com.sample.todoapp.model.Task;
 import com.sample.todoapp.service.TaskService;
 
@@ -61,5 +65,12 @@ public class TaskController {
 	public HttpStatus deleteAllTaskOfUser(@PathVariable long userid) {
 		taskService.deleteAllTaskOfUser(userid);
 		return HttpStatus.OK;
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public String handleResourceNotFoundException(Exception ex) {
+		return ex.getMessage();
 	}
 }
--- src/main/java/com/sample/todoapp/controller/UserController.java
+++ src/main/java/com/sample/todoapp/controller/UserController.java
@@ -1,21 +1,30 @@
 package com.sample.todoapp.controller;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.todoapp.exception.ExceptionResponse;
+import com.sample.todoapp.exception.UserNotFoundException;
 import com.sample.todoapp.model.User;
 import com.sample.todoapp.service.UserService;
 
@@ -57,4 +66,14 @@ public class UserController {
 		userService.deleteUser(id);
 		return HttpStatus.OK;
 	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ExceptionResponse handleUserNotFoundException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+		ExceptionResponse exResponse = new ExceptionResponse();
+		exResponse.setDateTime(LocalDateTime.now());
+		exResponse.setMessage(ex.toString());
+		return exResponse;
+    }
 }
